f1ed562d3aa7a2c97ea74fd9fba5e35c722d4bbe
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-04-06 17:40:06 -0300

    configurando rotas com react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { Switch, Router } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Switch>
+      <Router path="/" component={Login} />
+      <Router path="/home" component={Home} />
+    </Switch>
+  );
 }
 
 export default App;
